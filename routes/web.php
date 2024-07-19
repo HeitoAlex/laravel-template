59ff39ef0fb2d7189d8a5c1e3566b6b7c5ba2490
HeitoAlex <giallorossotuttalavita@hotmail.it>
2024-07-19 16:19:21 +0200

    aggiunto page controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Guest\PageController as GuestPageController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layouts.app');
-});
+Route::get('/', [GuestPageController::class, 'index'])->name('guest.home');
